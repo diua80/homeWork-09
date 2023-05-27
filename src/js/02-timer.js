a3a3dbc2ee6da23c9f4d467fcd944afdc8495bc6
Iryna Yatsenko <iryna.didova@gmail.com>
2023-05-27 14:23:46 +0300

    add resolve

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,12 +5,18 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 flatpickr("#datetime-picker", {
   enableTime: true,
   time_24hr: true,
-//   defaultDate: new Date(),
+  defaultDate: new Date(),
   minuteIncrement: 1,
+  
   onClose(selectedDates) {
-  console.log(selectedDates[0]);
-//   
-    },
+    const currentDate = new Date().getTime();
+    const selectedDate = selectedDates[0].getTime();
+
+    if (selectedDate <= currentDate) {
+      Notify.warning('Please choose a date in the future');
+      this.clear();
+    }
+  },
 });
 
 const refs = {
@@ -21,48 +27,35 @@ const refs = {
     seconds: document.getElementById('s'),
 }
 
-const datePicker = document.getElementById('datetime-picker');
 let countdown;
 
-
-
-datePicker.addEventListener('change', updateStartButtonState);
-updateStartButtonState();
-function updateStartButtonState() {
-    
-    const currentDate = new Date().getTime();
-
-    const selectedDate = new Date(datePicker.value).getTime();
-
-    if (selectedDate <= currentDate) {
-        Notify.warning('Please choose a date in the future');
-        refs.startBtn.disabled = true;
-    } else {
-        refs.startBtn.disabled = false;
-        }
+function pad(value) {
+  return value.toString().padStart(2, '0');
 }
 
-function startTimer () {
-    const selectedDate = new Date(datePicker.value).getTime();
-    countdown = setInterval(() => {
-        const now = new Date();
-        const distance = selectedDate - now;
-    
-        if (distance < 0) {
-            clearInterval(countdown);
-            return;
-        }
-    
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+function startTimer() {
+  const selectedDate = new Date(flatpickr("#datetime-picker").selectedDates[0]).getTime();
+  countdown = setInterval(() => {
+    const now = new Date();
+    const distance = selectedDate - now;
 
-        refs.days.textContent = days;
-        refs.hours.textContent = hours;
-        refs.minutes.textContent = minutes;
-        refs.seconds.textContent = seconds;
-    }, 1000);
+    if (distance < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    const days = pad(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const minutes = pad(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = pad(Math.floor((distance % (1000 * 60)) / 1000));
+
+    refs.days.textContent = pad(days);
+    refs.hours.textContent = pad(hours);
+    refs.minutes.textContent = pad(minutes);
+    refs.seconds.textContent = pad(seconds);
+  }, 1000);
 }
+
+
 
 refs.startBtn.addEventListener('click', startTimer);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,50 +7,33 @@ const refs = {
   amountInput: document.querySelector('input[name="amount"]'),
   submitButton: document.querySelector('button[type="submit"]'),
 }
-
 refs.form.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
   event.preventDefault();
-  
+
   let delay = parseInt(refs.delayInput.value);
   const step = parseInt(refs.stepInput.value);
   const amount = parseInt(refs.amountInput.value);
-  
-  const promises = createPromises(amount, delay, step);
 
-  Promise.all(promises.map((promise, index) => {
-    return promise
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const promise = createPromise(position, delay);
+
+    promise
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-  }))
-    // .catch(error => {
-    //   console.error('Error:', error);
-    // });
-}
 
-function createPromises(amount, delay, step) {
-  const promises = [];
-  // let delay = initialDelay;
-  
-  for (let i = 1; i <= amount; i++) {
-    const position = i;
-    const promise = createPromise(position, delay);
-    promises.push(promise);
     delay += step;
   }
-  
-  return promises;
 }
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
